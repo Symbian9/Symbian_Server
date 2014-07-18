b3922bccbf51e46c8293abe00244f59e124b84b5
bjbschmitt <bernardo.jb.schmitt@gmail.com>
2014-07-17 23:10:39 -0300

    Create get_IP.py
    Small script to get the current IP address from a Nokia Symbian^1/S60-5th edition mobile connected to an wi-fi lan.
    Requires the socket_tasks.py library.

---
+++ get_IP.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Small script to get the current IP address from a Nokia Symbian^1/S60-5th edition 
+mobile connected to an wi-fi lan.
+Requires the socket_tasks.py library.
+
+17/Jul/2014 Bernardo J.B. Schmitt - bernardo.jb.schmitt@gmail.com
+"""
+
+# Replace here with the path where the *.py files are saved.
+#--------------------------------
+file_path = "e:\\Python"
+#--------------------------------
+
+import sys
+sys.path.append(file_path)
+
+from socket_tasks import *
+from appuifw import *
+
+app.title = u"Get IP"
+apo = sel_access_point()
+apo.start()
+
+note(u"Cell IP: "+apo.ip(),"info")
